d9ece84ccaa2b240de2f67eeb0919124db0efaf4
BewiZ <121777666+BewiZ@users.noreply.github.com>
2024-05-11 21:56:17 +0800

    hanoi.js

--- Hanoi/hanoi.js
+++ Hanoi/hanoi.js
@@ -2,7 +2,6 @@
 var canvas = document.getElementById('canvas');
 var ca = canvas.getContext('2d');
 
-//一个简单的闭包实现计数器, 主要是为了给各个盘子命名用
 function counterfun() {
     var i = 0;
     return function () {
@@ -22,12 +21,11 @@ let speed = 20;
 
 var pans = [];
 
-//移动操作记录
-var operations = []; //存放移动操作记录
-var current_op = null; //当前操作
-var current_pan = null; //当前做移动动画的盘子
-var moving = false; //是否有盘子在做移动动画
-var current_op_index = 0; //指向operations的指针
+var operations = [];
+var current_op = null;
+var current_pan = null;
+var moving = false;
+var current_op_index = 0;
 
 var start = false;
 document.getElementById('start_btn').onclick = function (e) {
@@ -40,7 +38,6 @@ document.getElementById('start_btn').onclick = function (e) {
         pans.push(new pan(i * 20));
     }
 
-    // 将新的盘子压入p1柱子顶部
     pans.forEach(function (pan) {
         p1.addPan(pan);
     });
@@ -49,7 +46,6 @@ document.getElementById('start_btn').onclick = function (e) {
     window.requestAnimationFrame(main);
 };
 
-//重置按钮
 document.getElementById('reset_btn').onclick = function (e) {
     start = false;
     reset();
@@ -73,9 +69,8 @@ function randomColor() {
     return color;
 }
 
-//盘子
 function pan(r) {
-    this.stage = 0; //当前动画状态 0:停止, 1:上升, 2:平移, 3:下降
+    this.stage = 0; //0:停止, 1:上升, 2:平移, 3:下降
     this.x = 0;
     this.y = 0;
     this.color = randomColor();
@@ -108,29 +103,25 @@ function pan(r) {
         ca.stroke();
     };
 
-    //移动到指定位置
     this.moveTo = function (x, y) {
         this.x = x;
         this.y = y;
     };
 
-    //重置自身状体
     this.reset = function () {
         this.stage = 0;
     };
 
-    //移动动画中更新自己位置的函数
     this.moving = function (destin, cbk = null) {
         switch (this.stage) {
             case 0:
-                //停止状态下 如果是处于 moving 就跳到下一阶段, 否则直接返回
                 if (!moving) {
                     return;
                 } else {
                     this.stage = 1;
                 }
             case 1:
-                //上升阶段. y坐标不断减小, 到达最高点就进入下一阶段
+                //上
                 this.y -= speed;
                 if (this.y <= this.h) {
                     this.y = this.h;
@@ -138,7 +129,7 @@ function pan(r) {
                 }
                 break;
             case 2:
-                //平移阶段. 移动方向为相对位置差.  如果移动方向反向 就说明平移该结束了, 进入下一阶段
+                //平
                 var direct = this.x < destin.x ? 1 : -1;
                 this.x += direct * speed;
                 var direct_2 = this.x < destin.x ? 1 : -1;
@@ -148,7 +139,7 @@ function pan(r) {
                 }
                 break;
             case 3:
-                //下降阶段. y向到达目标柱子当前高度再加半个盘子高度就到位了. 加入到目标柱子执行回调.
+                //下
                 this.y += speed;
                 if (this.y >= destin.y - destin.current_h - this.h / 2) {
                     destin.addPan(this);
@@ -161,7 +152,6 @@ function pan(r) {
     };
 }
 
-//柱子
 function pillar(x, y, lab) {
     this.x = x;
     this.y = y;
@@ -197,7 +187,6 @@ function pillar(x, y, lab) {
     };
 }
 
-//绘制所有对象
 function draw() {
     var drawable = [p1, p2, p3];
     drawable.forEach(function (item) {
@@ -223,13 +212,11 @@ function move(n, source, destin, temp) {
     }
 }
 
-//盘子移动到目标柱子
 function movePan(pan, destin) {
     destin.addPan(pan);
     return;
 }
 
-//盘子移动动画执行完的回调
 function moveCallbk() {
     current_op = null;
     current_pan = null;
@@ -237,18 +224,14 @@ function moveCallbk() {
     current_op_index++;
 }
 
-//更新位置
 function update() {
     if (moving && current_pan != null) {
-        //如果有盘子处于运动中. 就继续更新其位置
         current_pan.moving(current_op[1], moveCallbk);
     } else {
-        //所有盘子都已经移动完毕就直接返回
         if (current_op_index >= operations.length) {
             start = false;
             return;
         } else {
-            //否则就把当前操作取出来,执行
             moving = true;
             current_op = operations[current_op_index];
             current_pan = current_op[0].popPan();
@@ -259,33 +242,26 @@ function update() {
     }
 }
 
-//重置所有对象状态
 function reset() {
     while (p1.popPan()) { }
     while (p2.popPan()) { }
     while (p3.popPan()) { }
 
     moving = false;
-    //重置
     pans.forEach(function (pan) {
         pan.reset();
         p1.addPan(pan);
     });
-    //当前操作指针指向头部
     current_op_index = 0;
 }
 
-//初始化.
 function init() {
-    //盘子都加入到A柱上
     pans.forEach(function (pan) {
         p1.addPan(pan);
     });
 
-    //执行递归主函数, 记录所有的移动操作
     move(p1, p1.pans.length, p2, p3);
 
-    //重置
     reset();
 }
 
